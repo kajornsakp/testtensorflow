54f15c4b5b3474985efb93e5880271035d7f5194
Kajornsak Peerapathananont <kajornsak_psm@hotmail.com>
2017-11-15 16:52:18 +0700

    updated keras model

--- coreml_converter.py
+++ coreml_converter.py
@@ -1,6 +1,19 @@
 import coremltools as coreml
+from keras.engine import InputLayer
 from keras.models import load_model
 
-model = coreml.converters.keras.convert("keras_model.h5")
-model.save('test_keras_datagen.mlmodel')
+output_labels =['0','1','2','3','4','5','6','7',
+                '8','9','10','11','12','13','14',
+                '15','16','17','18','19','20',
+                '21','22','23','24','25','26',
+                '27','28','29','30','31']
 
+
+coreml_model = coreml.converters.keras.convert('keras_model.h5',
+                                               input_names="image",
+                                               image_input_names='image',
+                                               output_names='output',
+                                               class_labels=output_labels,
+                                               image_scale=1/255.)
+
+coreml_model.save('keras_coreml.mlmodel')
--- keras_my_cnn.py
+++ keras_my_cnn.py
@@ -9,37 +9,60 @@ from keras.layers.normalization import BatchNormalization
 batch_size = 32
 num_classes = 32
 epochs = 200
+from keras.datasets import cifar10
 
+input_shape = (400,300,3)
 
-input_shape = (400,300)
-
-model = Sequential()
-model.add(Conv2D(32,(3,3),input_shape=(400,300,3),data_format='channels_first',activation='relu',name='conv1_1'))
-model.add(ZeroPadding2D((1,1)))
-model.add(MaxPooling2D(pool_size=2,strides=2,padding='same'))
-model.add(ZeroPadding2D((1,1)))
-
-model.add(Conv2D(32,(3,3),activation='relu',name='conv2_1'))
-model.add(ZeroPadding2D((1,1)))
-
-model.add(MaxPooling2D(pool_size=2,strides=2,padding='same'))
-model.add(ZeroPadding2D((1,1)))
-
-model.add(Conv2D(64,(3,3),activation='relu',name='conv_1'))
-model.add(ZeroPadding2D((1,1)))
-
-model.add(MaxPooling2D(pool_size=2,strides=2,padding='same'))
-model.add(ZeroPadding2D((1,1)))
-
-#bottle neck
-model.add(Flatten())
-model.add(Dense(256,activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(num_classes,activation='softmax'))
-# model.add(Activation('softmax'))
-model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-
-
+# model = Sequential()
+# model.add(Conv2D(32,(3,3),input_shape=(400,300,3),data_format='channels_first',activation='relu',name='conv1_1'))
+# model.add(ZeroPadding2D((1,1)))
+# model.add(MaxPooling2D(pool_size=2,strides=2,padding='same'))
+# model.add(ZeroPadding2D((1,1)))
+#
+# model.add(Conv2D(32,(3,3),activation='relu',name='conv2_1'))
+# model.add(ZeroPadding2D((1,1)))
+#
+# model.add(MaxPooling2D(pool_size=2,strides=2,padding='same'))
+# model.add(ZeroPadding2D((1,1)))
+#
+# model.add(Conv2D(64,(3,3),activation='relu',name='conv_1'))
+# model.add(ZeroPadding2D((1,1)))
+#
+# model.add(MaxPooling2D(pool_size=2,strides=2,padding='same'))
+# model.add(ZeroPadding2D((1,1)))
+#
+# #bottle neck
+# model.add(Flatten())
+# model.add(Dense(256,activation='relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(num_classes,activation='softmax'))
+# # model.add(Activation('softmax'))
+# model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+#
+model = Sequential([
+    Conv2D(64, (3, 3), input_shape=input_shape, padding='same',
+           activation='relu'),
+    Conv2D(64, (3, 3), activation='relu', padding='same'),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+    Conv2D(128, (3, 3), activation='relu', padding='same'),
+    Conv2D(128, (3, 3), activation='relu', padding='same',),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+    Conv2D(256, (3, 3), activation='relu', padding='same',),
+    Conv2D(256, (3, 3), activation='relu', padding='same',),
+    Conv2D(256, (3, 3), activation='relu', padding='same',),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+    Conv2D(512, (3, 3), activation='relu', padding='same',),
+    Conv2D(512, (3, 3), activation='relu', padding='same',),
+    Conv2D(512, (3, 3), activation='relu', padding='same',),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+    Conv2D(512, (3, 3), activation='relu', padding='same',),
+    Conv2D(512, (3, 3), activation='relu', padding='same',),
+    Conv2D(512, (3, 3), activation='relu', padding='same',),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
+    Flatten(),
+    Dense(256, activation='relu'),
+    Dense(num_classes, activation='softmax')
+])
 train_datagen = ImageDataGenerator(
     shear_range=0.2,
     zoom_range=0.2,
@@ -56,9 +79,8 @@ validation_generator = test_datagen.flow_from_directory('leaves_data/validate',
                                                         batch_size=batch_size,
                                                         class_mode='categorical')
 
-
-
-
+model.summary()
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 model.fit_generator(
     train_generator,
     steps_per_epoch=30,
@@ -66,5 +88,5 @@ model.fit_generator(
     validation_data=validation_generator,
     validation_steps=800 // batch_size
 )
-model.save('keras_model.h5')
-model.summary()
+model.save('keras_model_2.h5')
+# model.summary()
